0e6bce2a6884cde8317751bdd08c89ed102d3142
JV2008 <joaovictor25032@gmail.com>
2024-06-05 08:16:53 -0300

    primeiro commit

---
+++ Arraylist5.java
@@ -0,0 +1,30 @@
+package aula2905ofc;
+
+import java.util.ArrayList;
+
+public class Arraylist5 {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> numeros = new ArrayList <>();
+
+
+		numeros.add(1);
+		numeros.add(2);
+		numeros.add(3);
+		numeros.add(4);
+		numeros.add(5);
+
+		int soma= 0,numero=0;
+		for (Integer numero:numeros);{
+
+			soma = numeros;
+
+			System.out.println("Valor de numero é =" +numero);
+		}	
+
+		System.out.println(soma);
+
+	}
+
+}
